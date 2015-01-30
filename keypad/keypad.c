46a0cf1021d3e1ddb83d0c46fbb8bc164bd06938
Naman Nandan <namankt55@gmail.com>
2015-01-30 23:36:19 +0530

    Update keypad.c

--- keypad/keypad.c
+++ keypad/keypad.c
@@ -16,9 +16,9 @@ static ring_buffer key_event_buffer;
 static uint8_t state = NO_PUSH;
 //key lookup table
 static unsigned char key_table[4][4] = {{'0','1','2','3'},
-								  	  	{'4','5','6','7'},
-								  	  	{'8','9','A','B'},
-								  	  	{'C','D','E','F'}};
+					{'4','5','6','7'},
+					{'8','9','A','B'},
+					{'C','D','E','F'}};
 
 //functions
 //configure row pins as inputs and column pins as outputs
